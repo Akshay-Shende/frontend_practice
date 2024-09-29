b8cbfb08041fd6be3cff7c23ab5616fc45479765
hp <hp@DESKTOP-SVA3L1T>
2024-09-29 16:44:58 +0530

    changes for Mobile No

--- src/app/registration/page.js
+++ src/app/registration/page.js
@@ -1,34 +1,39 @@
-'use client';
-import { useState } from 'react';
-import useAuth from '@/appwriteServices/auth';
-import conf from '@/app/configue/configue';
-import { account } from '../appWrite';
+"use client";
+import { useState } from "react";
+import useAuth from "@/appwriteServices/auth";
+import conf from "@/app/configue/configue";
+import { account } from "../appWrite";
 
 const Page = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [name, setName] = useState('');
+  const [email, setEmail]       = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName]         = useState("");
+  const [mobile, setMobile]     = useState(null);
+
   const { registerUser } = useAuth();
 
   const handleSubmit = async (e) => {
-    e.preventDefault(); 
+    e.preventDefault();
 
-  let registerResult =  await registerUser(email,password,name);
-  console.log(registerResult);
+    let registerResult = await registerUser(email,mobile, password, name);
+    console.log(registerResult);
   };
 
   const logoutHandler = async () => {
     try {
-      await account.deleteSession('current');
+      await account.deleteSession("current");
       setLoggedInUser(null);
     } catch (error) {
-      console.error('Logout failed:', error);
+      console.error("Logout failed:", error);
     }
   };
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <form className="bg-white p-8 rounded shadow-md w-96" onSubmit={handleSubmit}>
+      <form
+        className="bg-white p-8 rounded shadow-md w-96"
+        onSubmit={handleSubmit}
+      >
         <h2 className="text-2xl font-bold mb-6 text-center">Registration</h2>
 
         <div className="mb-4">
@@ -68,6 +73,23 @@ const Page = () => {
             onChange={(e) => setEmail(e.target.value)}
           />
         </div>
+        <label
+            className="block text-sm font-medium text-gray-700 mb-1"
+            htmlFor="text"
+          >
+            Mobile Number
+          </label>
+        <input
+          type="tel"
+          id="mobile"
+          name="mobile"
+          required
+          pattern="+[0-9]{13}"
+          className="w-full p-2 border border-gray-300 rounded focus:outline-none focus:ring focus:ring-blue-500"
+          placeholder="Enter your mobile number"
+          value={mobile}
+          onChange={(e) => setMobile(e.target.value)}
+        />
 
         <div className="mb-4">
           <label
@@ -96,7 +118,7 @@ const Page = () => {
         </button>
 
         <p className="mt-4 text-center text-sm text-gray-600">
-          Don't have an account?{' '}
+          Don't have an account?{" "}
           <a href="#" className="text-blue-600 hover:underline">
             Sign up
           </a>
--- src/appwriteServices/auth.js
+++ src/appwriteServices/auth.js
@@ -1,10 +1,20 @@
 import { useState } from "react";
 import { account,ID } from "@/app/appWrite";
+import {Users,Databases , Client} from 'node-appwrite'
+import conf from "@/app/configue/configue";
 const useAuth = () => {
   const [loggedInUser, setLoggedInUser] = useState(null);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [name, setName] = useState("");
+
+const client = new Client()
+.setEndpoint(conf.appwriteUrl)
+.setProject(conf.appwriteProjectId)
+.setKey(conf.appwriteAPIKey)
+
+
+const users  = new Users(client);
 
   const login = async (email, password) => {
     try {
@@ -18,14 +28,10 @@ const useAuth = () => {
     }
   };
 
-  const registerUser = async (email, password, name) => { 
+  const registerUser = async (email,phone, password, name) => { 
     try {
-console.log("Email:", email);
-console.log("Password:", password);
-console.log("Name:", name);
-
-let phone = "+911234567890";
-      await account.create(ID.unique(), email, password, name);
+     // await account.create(ID.unique(), email, password, name);
+      await users.create(ID.unique(),email,phone,password,name)
       await login(email, password);
       // await account.updatePrefs({
       //   phoneNumber: phone
