9289cf2abbdb92580217cf60199daed8e7cbfcc2
hp <hp@DESKTOP-SVA3L1T>
2024-10-02 23:19:55 +0530

    build failed

--- src/_components/spinner.jsx
+++ src/_components/spinner.jsx
@@ -1,13 +1,9 @@
-import {useContext} from 'react'
-import {LoadingContext} from '@/context/loadingContext'
 
 const Spinner = () => {
-  const {loading, setLoading} = useContext(LoadingContext)
   return (
      <div className="flex justify-center items-center h-screen">
         <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
       </div>
   )
 }
-export {loading , setLoading}
 export default Spinner
