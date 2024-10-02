b2c7716ba35eb980a895461b110410c897a26a8d
hp <hp@DESKTOP-SVA3L1T>
2024-10-02 20:33:30 +0530

    change Header

--- src/_components/header.jsx
+++ src/_components/header.jsx
@@ -3,27 +3,31 @@ import { useEffect } from "react";
 import NavLink from "./navLink";
 import { useSelector, useDispatch } from "react-redux";
 import { useState } from "react";
-import { addLogInUser, deleteLogInUser } from "@/features/logInUser/logInUserSlice";
+import {
+  addLogInUser,
+  deleteLogInUser,
+} from "@/features/logInUser/logInUserSlice";
 import { account } from "@/app/appWrite";
 import { useRouter } from "next/navigation";
 
 const Header = () => {
-  const [userId, setUserId] = useState(0);
-  const user = useSelector((state) => state.logInUserReducer.user.id);
+  const [userData, setUserData] = useState(0);
+  const [firstName, setFirstName] = useState("");
+  const user = useSelector((state) => state.logInUserReducer.user);
   const dispatch = useDispatch();
   const router = useRouter();
 
   useEffect(() => {
-    console.log(user);
-    setUserId(user);
-  },[user]);
+    user.name ? setFirstName(user.name.split(" ")[0]) : setFirstName("");
+    setUserData(user);
+  }, [user]);
 
   const logoutHandler = async () => {
     try {
       await account.deleteSession("current");
-     dispatch(deleteLogInUser());
-     localStorage.removeItem("persist:root");
-     router.push("/login");
+      dispatch(deleteLogInUser());
+      localStorage.removeItem("persist:root");
+      router.push("/login");
     } catch (error) {
       console.error("Logout failed:", error);
     }
@@ -43,16 +47,21 @@ const Header = () => {
         </li>
         <div className="flex justify-end items-end">
           <li className="px-4 ">
-            <NavLink href={"/xyz"}>Home</NavLink>
+            <NavLink href={"/xyz"}>About Us</NavLink>
           </li>
-          {userId == 0 ? (
+          {userData.id == 0 ? (
             <li className="px-4">
               <NavLink href={"/login"}> Login</NavLink>
             </li>
           ) : (
-            <li className="px-4 cursor-pointer" onClick={logoutHandler}>
-               LogOut
-            </li>
+            <>
+              <li className="px-4 cursor-pointer" onClick={logoutHandler}>
+                Hello {firstName}
+              </li>
+              <li className="px-4 cursor-pointer" onClick={logoutHandler}>
+                Log Out
+              </li>
+            </>
           )}
         </div>
       </ul>
