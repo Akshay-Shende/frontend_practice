992fbfdef3ddda435afd4220f492cc14566f395a
hp <hp@DESKTOP-SVA3L1T>
2024-10-06 00:37:28 +0530

    UI changes

--- src/_components/card.jsx
+++ src/_components/card.jsx
@@ -1,23 +1,28 @@
-import useCart from "@/appwriteServices/cartServices";
 import useFiles from "@/appwriteServices/fileServices";
 import useWishlist from "@/appwriteServices/wishlistServices";
-import { useEffect } from "react";
 import { useSelector } from "react-redux";
+import { FaRegHeart } from "react-icons/fa";
+import { FaHeart } from "react-icons/fa";
+import { FaPlus } from "react-icons/fa";
+import { FaMinus } from "react-icons/fa6";
 
 const Card = ({ product }) => {
   const { getFilePreview } = useFiles();
-const {addToWishlist} = useWishlist();
-  const userId = useSelector(state => state.logInUserReducer.user.id);
+  const { addToWishlist } = useWishlist();
+  const userId = useSelector((state) => state.logInUserReducer.user.id);
 
   const addToWishlistFunction = () => {
     console.log("wishlist");
-    
-    const wishlist ={UserId : userId,ProductId : product.$id, AddedOn : Date.now()};
+
+    const wishlist = {
+      UserId: userId,
+      ProductId: product.$id,
+      AddedOn: Date.now(),
+    };
     try {
-   const resultData =  addToWishlist(wishlist);
-   return resultData;
-    }
-    catch (error) {
+      const resultData = addToWishlist(wishlist);
+      return resultData;
+    } catch (error) {
       console.log(error);
     }
   };
@@ -30,22 +35,28 @@ const {addToWishlist} = useWishlist();
         alt={product.ProductName}
       />
       <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">{product.ProductName}</div>
+        <div className="font-bold text-xl mb-2 flex">
+          {product.ProductName}
+
+          <button className="ms-36">
+            <FaRegHeart
+              style={{ marginTop: "5px", color: "red", fontSize: "24px" }}
+            />
+          </button>
+        </div>
         <p className="text-gray-700 text-base">{product.ProductDescription}</p>
       </div>
       <div className="px-6 pt-4 pb-2">
+
+    <div className="flex justify-between py-3 px-4">
+      <button><FaPlus/></button>
+      <span>2</span>
+      <button><FaMinus/></button>
+    </div>
+
         <div className="pb-2">
-          <button className="py-1 px-2 me-6 bg-blue-600 text-white rounded hover:bg-blue-700 transition duration-200"
-          
-          >
+          <button className="py-1 px-2 me-6 bg-blue-600 text-white rounded hover:bg-blue-700 transition duration-200">
             Add to Cart
-          </button>
-          <button className="py-1 px-2 bg-yellow-500 text-white rounded hover:bg-yellow-600 transition duration-200"
-          onClick={
-            addToWishlistFunction
-          }
-          >
-            Wishlist
           </button>
         </div>
       </div>
